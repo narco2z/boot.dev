762ced9cc8bb3f6509b7703eeeee77ce241c2790
narco2z <75574844+narco2z@users.noreply.github.com>
2024-03-28 16:49:01 +0100

    added LeafNode class

--- static_site_gen/src/htmlnode.py
+++ static_site_gen/src/htmlnode.py
@@ -1,6 +1,6 @@
 class HTMLNode:
 
-    def __init__(self, tag=None, value=None, children=None, props=None):
+    def __init__(self, tag: str=None, value:str=None, children:list=None, props:dict=None):
         self.tag = tag
         self.value = value
         self.children = children
@@ -10,10 +10,12 @@ class HTMLNode:
         raise NotImplementedError()
 
     def props_to_html(self):
-        html = []
-        for key, value in self.props.items():
-            html.append(f' {key}="{value}"')
-        return "".join(html)
+        if self.props is not None:
+            html = []
+            for key, value in self.props.items():
+                html.append(f' {key}="{value}"')
+            return "".join(html)
+        return ''
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
@@ -21,7 +23,12 @@ class HTMLNode:
 
 class LeafNode(HTMLNode):
     """docstring for LeafNode."""
-    def __init__(self, value):
-        super(LeafNode, self).__init__()
-        self.arg = arg
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
         
+    def to_html(self):
+        if self.value is None:
+            raise ValueError()
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
--- static_site_gen/src/test_htmlnode.py
+++ static_site_gen/src/test_htmlnode.py
@@ -1,12 +1,21 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode("a", "test", [None], {"href": "https://www.boot.dev", "target": "_blank"})
         self.assertEqual(node.props_to_html(), ' href="https://www.boot.dev" target="_blank"')
+
+    def test_to_html(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node.to_html(), '<p>This is a paragraph of text.</p>')
+
+    def test_to_html2(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
 
 
 if __name__ == "__main__":
